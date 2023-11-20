b2a1d07efd93c7ff972308e5cc7f131dcfd32904
Franklin Michael Tantacure Guevara <ftantacure@esan.edu.pe>
2023-11-20 12:23:39 -0500

    Se agrega el Ajax

--- ProyectoDos/Proyecto2Demo/Controllers/HomeController.cs
+++ ProyectoDos/Proyecto2Demo/Controllers/HomeController.cs
@@ -18,6 +18,62 @@ namespace Proyecto2Demo.Controllers
             return View();
         }
 
+        [HttpPost]
+        public ActionResult GuardarDatos(string username, string password)
+        {
+            var user = "123";
+            var pass = "456";
 
+            if (user == username && pass == password)
+            {
+                //Session["admin"] = username;
+                return Json(1);
+
+            }
+            else
+            {
+                return Json(0);
+            }
+
+            
+        }
+
+        //[HttpPost]
+        //public ActionResult GuardarDatos(string username, string password)
+        //{
+        //    var user = "123";
+        //    var pass = "456";
+
+        //    if (user == username && pass == password)
+        //    {
+        //        Session["admin"] = username;
+        //        return Json(1);
+        //    }
+        //    else
+        //    {
+        //        return Json(0);
+        //    }
+
+
+        //}
+
+
+        //[HttpPost]
+        //public JsonResult GuardarDatos(string username, string password)
+        //{
+        //    var user = "123";
+        //    var pass = "456";
+        //    var response = "";
+        //    if (user == username && pass == password)
+        //    {
+        //        response = "Datos recibidos con éxito. Usuario: " + username + ", Contraseña: " + password;
+        //    }
+        //    else
+        //    {
+        //        response = "Datos incorrectos: " + username + ", Contraseña: " + password;
+        //    }
+
+        //    return Json(response, JsonRequestBehavior.AllowGet);
+        //}
     }
 }
